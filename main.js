59279d62fb4f7930d7b01d7ece3dcf4fb6287503
Lockol <71211425+Lockol@users.noreply.github.com>
2023-04-14 18:38:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// 2
+// alert('Priadytskyi');
+
+// 6
+// let myName = "Alex";
+// let surName = 'Priadytskyi';
+// let className = 'JS fundamental';
+// let year = 1993;
+// let married = confirm('Are you married?');
+
+// console.log(`Year: ${year}, type: ${typeof (year)}`);
+// console.log(`Married: ${married}, type: ${typeof (married)}`)
+// console.log(`Name: ${myName}, type: ${typeof (myName)}`);
+// console.log(`Surname: ${surName}, type: ${typeof (surName)}`);
+// console.log(`Classname: ${className}, type: ${typeof (className)}`);
+
+// let x = null;
+// let y ;
+// console.log(x);
+// console.log(y);
+
+
